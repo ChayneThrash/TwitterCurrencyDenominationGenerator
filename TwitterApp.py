8ddec313f9e2803be26001c793ce66dfa0cdae6f
Chayne Thrash <chaynethrash@gmail.com>
2015-07-14 19:47:21 -0500

    Refactoring project
    Created application class and separated configuration parameters into
    Settings file

--- TwitterApp.py
+++ TwitterApp.py
@@ -4,27 +4,40 @@ import json
 from requests_oauthlib import OAuth1Session
 from requests_oauthlib import OAuth1
 from CurrencyDenominationGenerator import *
+import Settings
 
-TWITTER_USER_NAME = 'smith2015_kevin'
-CONSUMER_KEY = '2xbz3fkDvZl57K7jNv30jMgCT'
-CONSUMER_SECRET = '3MmmeDnMuN9mUV6kCw3IAYfbxeWLl5J2fYDIiaJV40fPFvwTK8'
-TOKEN_KEY = '3340282143-c3pn0lkxxOXll5ZReY6TyR5UWHv4ssbjy5H5ktF'
-TOKEN_SECRET = 'UncL8lPDGdjCE1NaTlCAmQDAcXhCFuDlVhb60dKr8V31c'
 
-oauth = OAuth1(CONSUMER_KEY, client_secret=CONSUMER_SECRET, resource_owner_key=TOKEN_KEY, resource_owner_secret=TOKEN_SECRET, signature_type='auth_header')
+class CurrencyDenominationTwitterApp:
+    def __init__(self):
+        self.oauth = OAuth1(Settings.CONSUMER_KEY, client_secret=Settings.CONSUMER_SECRET, resource_owner_key=Settings.TOKEN_KEY,
+                            resource_owner_secret=Settings.TOKEN_SECRET, signature_type='auth_header')
 
-stream_url = 'https://userstream.twitter.com/1.1/user.json'
-direct_message_url = 'https://api.twitter.com/1.1/direct_messages/new.json'
-direct_message_params = {'user_id' : 'temporary id', 'text' : 'temporary text'}
+    def runApp(self):
+        stream = self.__connectToStream()
+        for line in stream.iter_lines():
+            if line:  # filter out keep-alive new lines
+                print line
+                self.__processResponse(json.loads(line))
 
-r = requests.post(stream_url, auth=oauth, stream=True)
+    def __connectToStream(self):
+        stream_url = 'https://userstream.twitter.com/1.1/user.json'
+        return requests.post(stream_url, auth=self.oauth, stream=True)
 
-for line in r.iter_lines():
-    if line: # filter out keep-alive new lines
-        stream_resp = json.loads(line)
-        if ('direct_message' in stream_resp) and (stream_resp['direct_message']['sender']['screen_name'] != TWITTER_USER_NAME):
-            message = stream_resp['direct_message']
-            direct_message_params['text'] = generateCurrencyDenominationsString(message['text'])
-            direct_message_params['user_id'] = stream_resp['direct_message']['sender']['id']
-            requests.post(direct_message_url, params=direct_message_params, auth=oauth)
-        print(line)
+    def __processResponse(self, response):
+        if ('direct_message' in response) and (self.__senderIsNotApp(response)):
+            received_message = response['direct_message']
+            self.__respond(received_message)
+
+    def __senderIsNotApp(self, direct_message):
+        return direct_message['direct_message']['sender']['screen_name'] != Settings.TWITTER_USER_NAME
+
+    def __respond(self, received_message):
+        direct_message_url = 'https://api.twitter.com/1.1/direct_messages/new.json'
+        direct_message_params = dict(text = generateCurrencyDenominationsString(received_message['text']),
+                                     user_id = received_message['sender']['id'])
+        requests.post(direct_message_url, params=direct_message_params, auth=self.oauth)
+
+
+
+if __name__ == '__main__':
+    CurrencyDenominationTwitterApp().runApp()
