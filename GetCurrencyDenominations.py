f389130368b04543a9ec64bdd653de9fda1cdcfd
Chayne Thrash <chaynethrash@gmail.com>
2015-07-15 17:45:40 -0500

    Made console application if ran independently. Also made it so that values that are zero are not returned in the string

--- GetCurrencyDenominations.py
+++ GetCurrencyDenominations.py
@@ -1,6 +1,27 @@
 __author__ = 'Chayne'
 
 
+def generateDenominations(denomination, update_value):
+    num_denominations = update_value // denomination
+    update_value -= num_denominations * denomination
+    return (num_denominations, update_value)
+
+
+class OutputFormatter:
+    def __init__(self):
+        self.beginning_of_output = True
+
+    def getFormattedOutput(self, value_string, value):
+        formatted_output = ''
+        if value != 0:
+            if not self.beginning_of_output:
+                formatted_output += ', '
+            else:
+                self.beginning_of_output = False
+            return formatted_output + value_string + str(value)
+        return ''
+
+
 def generateCurrencyDenominationsString(currencyMessage):
     if currencyMessage[0] != '$':
         return "Currency must begin with a dollar sign"
@@ -17,26 +38,33 @@ def generateCurrencyDenominationsString(currencyMessage):
     except:
         return "Invalid character detected."
 
-    print(dollars)
-    print(cents)
+    num_of_hundreds, updated_dollars = generateDenominations(100, dollars)
+    num_of_twenties, updated_dollars = generateDenominations(20, updated_dollars)
+    num_of_tens, updated_dollars = generateDenominations(10, updated_dollars)
+    num_of_fives, updated_dollars = generateDenominations(5, updated_dollars)
+    num_of_ones = updated_dollars
 
-    num_of_hundreds = dollars//100
-    dollars -= num_of_hundreds*100
-    num_of_twenties = dollars//20
-    dollars -= num_of_twenties*20
-    num_of_tens = dollars//10
-    dollars -= num_of_tens*10
-    num_of_fives = dollars//5
-    num_of_ones = dollars - num_of_fives*5
+    num_of_quarters, updated_cents = generateDenominations(25, cents)
+    num_of_dimes, updated_cents = generateDenominations(10, updated_cents)
+    num_of_nickels, updated_cents = generateDenominations(5, updated_cents)
+    num_of_pennies = updated_cents
 
-    num_of_quarters = cents//25
-    cents -= num_of_quarters*25
-    num_of_dimes = cents//10
-    cents -= num_of_dimes*10
-    num_of_nickels = cents//5
-    num_of_pennies = cents - num_of_nickels*5
+    formatter = OutputFormatter()
 
-    formatted_output = "100's: " + str(num_of_hundreds) + ", 20's: " + str(num_of_twenties) + ", 10's: " + str(num_of_tens) + ", 5's: " + str(num_of_fives) + ", 1's: " + str(num_of_ones)
-    formatted_output += ", Q: " + str(num_of_quarters) + ", D: " + str(num_of_dimes) + ", N: " + str(num_of_nickels) + ", P: " + str(num_of_pennies)
+    formatted_output = formatter.getFormattedOutput("100's: ", num_of_hundreds)
+    formatted_output += formatter.getFormattedOutput("20's: ", num_of_twenties)
+    formatted_output += formatter.getFormattedOutput("10's: ", num_of_tens)
+    formatted_output += formatter.getFormattedOutput("5's: ", num_of_fives)
+    formatted_output += formatter.getFormattedOutput("1's: ", num_of_ones)
+
+    formatted_output += formatter.getFormattedOutput("Q's: ", num_of_quarters)
+    formatted_output += formatter.getFormattedOutput("D's: ", num_of_dimes)
+    formatted_output += formatter.getFormattedOutput("N's: ", num_of_nickels)
+    formatted_output += formatter.getFormattedOutput("P's: ", num_of_pennies)
 
     return formatted_output
+
+
+if __name__ == '__main__':
+    user_currency = raw_input('Enter a currency amount: ')
+    print generateCurrencyDenominationsString(user_currency)
