d3de10ae1ca5e9180eeccf809ff7297ae39762ce
Chayne Thrash <chaynethrash@gmail.com>
2015-07-16 18:51:02 -0500

    Name changes and 503 error code added

--- GetChangeAmounts.py
+++ GetChangeAmounts.py
@@ -3,7 +3,7 @@ __author__ = 'Chayne'
 
 # Generates the number of denominations in the value given. It also returns the value passed in after removing
 # all of the denominations. ie: updated_val // denomination will be equal.
-def generateDenominations(denomination, update_value):
+def getDenominations(denomination, update_value):
     num_denominations = update_value // denomination
     update_value -= num_denominations * denomination
     return num_denominations, update_value
@@ -12,43 +12,43 @@ def generateDenominations(denomination, update_value):
 class OutputFormatter:
     def __init__(self):
         self.beginning_of_output = True
-        self.formattedOutput = ''
+        self.formatted_output = ''
 
     # This function adds the string and value to the output. Can be chained
     # so that multiple calls on separate lines are not necessary.
     def addToOutput(self, value_string, value):
         if value != 0:
             if not self.beginning_of_output:
-                self.formattedOutput += ', '
+                self.formatted_output += ', '
             else:
                 self.beginning_of_output = False
-            self.formattedOutput += value_string + str(value)
+            self.formatted_output += value_string + str(value)
         return self
 
     def getFormattedOutput(self):
-        return self.formattedOutput
+        return self.formatted_output
 
 
-# Class responsible for generating currency denominations. Also contains a formatted string for representing the
-# currency denominations.
-class CurrencyDenominationGenerator:
+# Class responsible for generating change given a currency amount in dollars and cents. Also contains a formatted
+# string for representing the currency denominations.
+class ChangeGenerator:
     def __init__(self, dollars, cents):
-        self.__generateDenominations(dollars, cents)
-        self.__formatCurrencyDenominations()
+        self.__generateChange(dollars, cents)
+        self.__formatChange()
 
-    def __generateDenominations(self, dollars, cents):
-        self.num_of_hundreds, updated_dollars = generateDenominations(100, dollars)
-        self.num_of_twenties, updated_dollars = generateDenominations(20, updated_dollars)
-        self.num_of_tens, updated_dollars = generateDenominations(10, updated_dollars)
-        self.num_of_fives, updated_dollars = generateDenominations(5, updated_dollars)
+    def __generateChange(self, dollars, cents):
+        self.num_of_hundreds, updated_dollars = getDenominations(100, dollars)
+        self.num_of_twenties, updated_dollars = getDenominations(20, updated_dollars)
+        self.num_of_tens, updated_dollars = getDenominations(10, updated_dollars)
+        self.num_of_fives, updated_dollars = getDenominations(5, updated_dollars)
         self.num_of_ones = updated_dollars
 
-        self.num_of_quarters, updated_cents = generateDenominations(25, cents)
-        self.num_of_dimes, updated_cents = generateDenominations(10, updated_cents)
-        self.num_of_nickels, updated_cents = generateDenominations(5, updated_cents)
+        self.num_of_quarters, updated_cents = getDenominations(25, cents)
+        self.num_of_dimes, updated_cents = getDenominations(10, updated_cents)
+        self.num_of_nickels, updated_cents = getDenominations(5, updated_cents)
         self.num_of_pennies = updated_cents
 
-    def __formatCurrencyDenominations(self):
+    def __formatChange(self):
         formatter = OutputFormatter()
 
         formatter.addToOutput("100's: ", self.num_of_hundreds).addToOutput("20's: ", self.num_of_twenties) \
@@ -63,27 +63,29 @@ class CurrencyDenominationGenerator:
         return self.formatted_currency_denominations
 
 
-def generateCurrencyDenominationsString(currency):
+def generateChangeString(currency):
     if currency[0] != '$':
-        return "Currency must begin with a dollar sign"
+        return 'Currency must begin with a dollar sign'
 
     currency = currency[1:]  # Strip off the dollar sign.
     currency = currency.split('.')
 
     if len(currency) > 2:
-        return "Invalid number of decimals"
+        return 'Invalid number of decimals'
+    if len(currency[1]) != 2:
+        return 'Invalid number of values after decimal.'
 
     try:
         dollars = int(currency[0])
         cents = int(currency[1])
     except:
-        return "Invalid character detected."
+        return 'Invalid character detected.'
 
-    denominations = CurrencyDenominationGenerator(dollars, cents)
+    denominations = ChangeGenerator(dollars, cents)
 
     return str(denominations)
 
 
 if __name__ == '__main__':
     user_currency = raw_input('Enter a currency amount: ')
-    print generateCurrencyDenominationsString(user_currency)
+    print generateChangeString(user_currency)
--- TwitterApp.py
+++ TwitterApp.py
@@ -1,13 +1,14 @@
 __author__ = 'Chayne'
 import requests, json, sys
 from requests_oauthlib import OAuth1
-from GetCurrencyDenominations import *
+from GetChangeAmounts import generateChangeString
 import Settings
 from time import sleep
 from ssl import SSLError
+import socket
 
 
-class CurrencyDenominationTwitterApp:
+class ChangeGeneratorTwitterApp:
     def __init__(self):
         self.oauth = OAuth1(Settings.CONSUMER_KEY, client_secret=Settings.CONSUMER_SECRET,
                             resource_owner_key=Settings.TOKEN_KEY,
@@ -16,7 +17,7 @@ class CurrencyDenominationTwitterApp:
         self.network_error_delay = 0
         self.general_http_error_delay = 5
         self.http_420_error_delay = 60
-        self.num_general_http_errors = 0
+        self.num_server_overload_errors = 0
         self.num_420_errors = 0
 
     def runApp(self):
@@ -28,16 +29,23 @@ class CurrencyDenominationTwitterApp:
                 for line in stream.iter_lines():
                     if line:  # filter out keep-alive new lines
                         stream.raise_for_status()  # if an http error occurs during streaming, raise an exception
+                        print line
                         self.__processResponse(json.loads(line))
 
-            except (requests.ConnectionError, SSLError):
+            except (requests.ConnectionError, SSLError, socket.error):
+                print 'TCP/IP error has occurred. Attempting to reconnect.'
                 self.last_error = 'TCP/IP'
                 continue
             except requests.HTTPError:
                 if stream.status_code == 420:
                     self.last_error = '420'
-                else:
-                    self.last_error = 'Gen Error'
+                elif stream.status_code == 503:
+                    print '503 error has occurred. Attempting to reconnect.'
+                    self.last_error = 'Server Overload'
+                else: # Twitter doesn't like something we are sending. Must be fixed.
+                    print 'The following HTTP error code was received: ', stream.status_code
+                    print 'This error must be fixed. Application cannot connect.'
+                    sys.exit(1)
                 continue
             except Exception as ex:
                 print 'The following unexpected error occurred:'
@@ -45,7 +53,7 @@ class CurrencyDenominationTwitterApp:
                 sys.exit(1)
 
     def __sleepBasedOnLastError(self):
-        if self.last_error == None:
+        if self.last_error is None:
             return
         elif self.last_error == 'TCP/IP':
             sleep(self.network_error_delay)
@@ -68,19 +76,19 @@ class CurrencyDenominationTwitterApp:
         return requests.get(stream_url, auth=self.oauth, stream=True, timeout=90)
 
     def __processResponse(self, response):
-        if ('direct_message' in response) and (self.__senderIsNotApp(response)):
+        if ('direct_message' in response) and (self.__messageIsFromOtherUser(response)):
             received_message = response['direct_message']
             self.__respond(received_message)
 
-    def __senderIsNotApp(self, direct_message):
+    def __messageIsFromOtherUser(self, direct_message):
         return direct_message['direct_message']['sender']['screen_name'] != Settings.TWITTER_USER_NAME
 
     def __respond(self, received_message):
         direct_message_url = 'https://api.twitter.com/1.1/direct_messages/new.json'
-        direct_message_params = dict(text=generateCurrencyDenominationsString(received_message['text']),
+        direct_message_params = dict(text=generateChangeString(received_message['text']),
                                      user_id=received_message['sender']['id'])
         requests.post(direct_message_url, params=direct_message_params, auth=self.oauth)
 
 
 if __name__ == '__main__':
-    CurrencyDenominationTwitterApp().runApp()
+    ChangeGeneratorTwitterApp().runApp()
