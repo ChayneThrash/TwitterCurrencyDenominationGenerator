21abfd7dde99e5b6096121dad24464158099a9b6
Chayne Thrash <chaynethrash@gmail.com>
2015-07-15 20:11:15 -0500

    Refactored GetCurrencyDenomations and added comments.

--- GetCurrencyDenominations.py
+++ GetCurrencyDenominations.py
@@ -1,68 +1,87 @@
 __author__ = 'Chayne'
 
 
+# Generates the number of denominations in the value given. It also returns the value passed in after removing
+# all of the denominations. ie: updated_val // denomination will be equal.
 def generateDenominations(denomination, update_value):
     num_denominations = update_value // denomination
     update_value -= num_denominations * denomination
-    return (num_denominations, update_value)
+    return num_denominations, update_value
 
-
+# Class responsible for formatting string specifically for currency denominations.
 class OutputFormatter:
     def __init__(self):
         self.beginning_of_output = True
+        self.formattedOutput = ''
 
-    def getFormattedOutput(self, value_string, value):
-        formatted_output = ''
+    # This function adds the string and value to the output. Can be chained
+    # so that multiple calls on separate lines are not necessary.
+    def addToOutput(self, value_string, value):
         if value != 0:
             if not self.beginning_of_output:
-                formatted_output += ', '
+                self.formattedOutput += ', '
             else:
                 self.beginning_of_output = False
-            return formatted_output + value_string + str(value)
-        return ''
+            self.formattedOutput += value_string + str(value)
+        return self
+
+    def getFormattedOutput(self):
+        return self.formattedOutput
 
 
-def generateCurrencyDenominationsString(currencyMessage):
-    if currencyMessage[0] != '$':
+# Class responsible for generating currency denominations. Also contains a formatted string for representing the
+# currency denominations.
+class CurrencyDenominationGenerator:
+    def __init__(self, dollars, cents):
+        self.__generateDenominations(dollars, cents)
+        self.__formatCurrencyDenominations()
+
+    def __generateDenominations(self, dollars, cents):
+        self.num_of_hundreds, updated_dollars = generateDenominations(100, dollars)
+        self.num_of_twenties, updated_dollars = generateDenominations(20, updated_dollars)
+        self.num_of_tens, updated_dollars = generateDenominations(10, updated_dollars)
+        self.num_of_fives, updated_dollars = generateDenominations(5, updated_dollars)
+        self.num_of_ones = updated_dollars
+
+        self.num_of_quarters, updated_cents = generateDenominations(25, cents)
+        self.num_of_dimes, updated_cents = generateDenominations(10, updated_cents)
+        self.num_of_nickels, updated_cents = generateDenominations(5, updated_cents)
+        self.num_of_pennies = updated_cents
+
+    def __formatCurrencyDenominations(self):
+        formatter = OutputFormatter()
+
+        formatter.addToOutput("100's: ", self.num_of_hundreds).addToOutput("20's: ", self.num_of_twenties) \
+            .addToOutput("10's: ", self.num_of_tens).addToOutput("5's: ", self.num_of_fives) \
+            .addToOutput("1's: ", self.num_of_ones)
+
+        formatter.addToOutput("Q's: ", self.num_of_quarters).addToOutput("D's: ", self.num_of_dimes) \
+            .addToOutput("N's: ", self.num_of_nickels).addToOutput("P's: ", self.num_of_pennies)
+        self.formatted_currency_denominations = formatter.getFormattedOutput()
+
+    def __str__(self):
+        return self.formatted_currency_denominations
+
+
+def generateCurrencyDenominationsString(currency):
+    if currency[0] != '$':
         return "Currency must begin with a dollar sign"
 
-    currency = currencyMessage[1:]
-    currencySplit = currency.split('.')
+    currency = currency[1:]  # Strip off the dollar sign.
+    currency = currency.split('.')
 
-    if (len(currencySplit) > 2):
+    if len(currency) > 2:
         return "Invalid number of decimals"
 
     try:
-        dollars = int(currencySplit[0])
-        cents = int(currencySplit[1])
+        dollars = int(currency[0])
+        cents = int(currency[1])
     except:
         return "Invalid character detected."
 
-    num_of_hundreds, updated_dollars = generateDenominations(100, dollars)
-    num_of_twenties, updated_dollars = generateDenominations(20, updated_dollars)
-    num_of_tens, updated_dollars = generateDenominations(10, updated_dollars)
-    num_of_fives, updated_dollars = generateDenominations(5, updated_dollars)
-    num_of_ones = updated_dollars
+    denominations = CurrencyDenominationGenerator(dollars, cents)
 
-    num_of_quarters, updated_cents = generateDenominations(25, cents)
-    num_of_dimes, updated_cents = generateDenominations(10, updated_cents)
-    num_of_nickels, updated_cents = generateDenominations(5, updated_cents)
-    num_of_pennies = updated_cents
-
-    formatter = OutputFormatter()
-
-    formatted_output = formatter.getFormattedOutput("100's: ", num_of_hundreds)
-    formatted_output += formatter.getFormattedOutput("20's: ", num_of_twenties)
-    formatted_output += formatter.getFormattedOutput("10's: ", num_of_tens)
-    formatted_output += formatter.getFormattedOutput("5's: ", num_of_fives)
-    formatted_output += formatter.getFormattedOutput("1's: ", num_of_ones)
-
-    formatted_output += formatter.getFormattedOutput("Q's: ", num_of_quarters)
-    formatted_output += formatter.getFormattedOutput("D's: ", num_of_dimes)
-    formatted_output += formatter.getFormattedOutput("N's: ", num_of_nickels)
-    formatted_output += formatter.getFormattedOutput("P's: ", num_of_pennies)
-
-    return formatted_output
+    return str(denominations)
 
 
 if __name__ == '__main__':
